15b05808df650b8a7dc0b4a9f63e393922c775d4
luiz-divino <nandodivinodev@gmail.com>
2026-07-12 16:44:01 -0300

    feat: create types for churchEvent  properties

---
+++ src/interfaces/church_event/churchEvent.ts
@@ -0,0 +1,8 @@
+export interface ChurchEventProps {
+      id: string;
+      title: string;
+      description?: string;
+      eventDate: Date;
+      createdBy: string;
+      createdAt: Date;
+}
