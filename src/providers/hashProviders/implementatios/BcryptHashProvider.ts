4bb2bcbfb4b5040efc76a1bd3fd7b8dd2fc035d2
luiz-divino <nandodivinodev@gmail.com>
2026-07-13 14:54:20 -0300

    feat: create a class responsible for hashing the password provided by the user and comparing the passwords at login

---
+++ src/providers/hashProviders/implementatios/BcryptHashProvider.ts
@@ -0,0 +1,11 @@
+import { compare, hash } from "bcryptjs";
+import { IHashProvider } from "../IHashProvider";
+
+export class BcryptHashProvider implements IHashProvider {
+      async generateHash(payload: string): Promise<string> {
+            return hash(payload, 8);
+      }
+      async compareHash(payload: string, hashed: string): Promise<boolean> {
+            return compare(payload, hashed);
+      }
+}
