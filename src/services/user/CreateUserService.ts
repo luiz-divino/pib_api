47fd8d58c815ee671ab06620046ad21855016597
luiz-divino <nandodivinodev@gmail.com>
2026-07-13 14:49:38 -0300

    feat: create user service
    - inject the repository responsible for the service into the constructor
    - inject the provider responsible for generating the password hash in the constructor
    - create and implement DTO interface responsible for ensuring data coming from the user controller
    - create and implement responsive response interface to ensure that the request does not return the password
    - generate the hash and save the registered user's data in the repository

---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,52 @@
+import { User } from "../../entities/User";
+import { UserRoleType } from "../../interfaces/user";
+import { BcryptHashProvider } from "../../providers/hashProviders/implementatios/BcryptHashProvider";
+import { IMemoryUserRepository } from "../../repositories/user/CreateUserRepository";
+
+interface UserDto {
+      name: string;
+      email: string;
+      password: string;
+      role?: UserRoleType;
+}
+
+type CreateUserResponse = {
+      id: string;
+      name: string;
+      email: string;
+      role: string;
+};
+
+export class CreateUserService {
+      constructor(
+            private userRepository: IMemoryUserRepository,
+            private hashProvider: BcryptHashProvider,
+      ) {}
+
+      async execute(props: UserDto): Promise<CreateUserResponse> {
+            const userAlReadyExists = await this.userRepository.findByEmail(
+                  props.email,
+            );
+            if (userAlReadyExists) {
+                  throw new Error("Este Email já está cadastrado no sistema");
+            }
+
+            const passwordHashed = await this.hashProvider.generateHash(
+                  props.password,
+            );
+            const user = User.create({
+                  name: props.name,
+                  email: props.email,
+                  password: passwordHashed,
+                  role: props.role as UserRoleType,
+            });
+
+            await this.userRepository.save(user);
+            return {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                  role: user.role,
+            };
+      }
+}
