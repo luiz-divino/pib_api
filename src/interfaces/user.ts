4d41de48525584eb6bc1b590aee5cf3d4c43742d
luiz-divino <nandodivinodev@gmail.com>
2026-07-12 21:11:01 -0300

    refactor: delete property that it's not used

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -2,10 +2,8 @@ import { randomUUID } from "crypto";
 
 export type UserRoleType = "MEMBER" | "FINANCE" | "ADMIN";
 export interface UserProps {
-      id: string;
       name: string;
       email: string;
       passwordHash: string;
       _role?: UserRoleType;
-      createdAt: Date;
 }
