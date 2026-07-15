826acf03f55cf732cc84897fb5f09e9b6ba84cea
luiz-divino <nandodivinodev@gmail.com>
2026-07-15 20:23:42 -0300

    chore: create interface props

---
+++ src/interfaces/event/event.ts
@@ -0,0 +1,8 @@
+export interface ChurchEventProps {
+      title: string;
+      description?: string | null
+      location: string;
+      eventDate: Date;
+      createdBy: string;
+      createdAt: Date;
+}
