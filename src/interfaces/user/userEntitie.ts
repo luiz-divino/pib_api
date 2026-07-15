45d57904f6dd2c6217ee28d234d6aedd24c30182
luiz-divino <nandodivinodev@gmail.com>
2026-07-14 21:02:07 -0300

    chore: adjust interface property

---
+++ src/interfaces/user/userEntitie.ts
@@ -0,0 +1,9 @@
+import { randomUUID } from "crypto";
+
+export type UserRoleType = "MEMBER" | "FINANCE" | "ADMIN";
+export interface UserProps {
+      name: string;
+      email: string;
+      password: string;
+      role?: UserRoleType;
+}
