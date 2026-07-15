dd264bd7985c4f85270c64e4902a8dc6881340f7
luiz-divino <nandodivinodev@gmail.com>
2026-07-14 21:17:39 -0300

    chore: change folder origin

--- src/interfaces/user.ts
+++
@@ -1,9 +0,0 @@
-import { randomUUID } from "crypto";
-
-export type UserRoleType = "MEMBER" | "FINANCE" | "ADMIN";
-export interface UserProps {
-      name: string;
-      email: string;
-      passwordHash: string;
-      _role?: UserRoleType;
-}
