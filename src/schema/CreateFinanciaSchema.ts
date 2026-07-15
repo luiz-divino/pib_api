7fd02bb1b6c98dc691d2628391c0c5a0ad468255
luiz-divino <nandodivinodev@gmail.com>
2026-07-14 21:15:40 -0300

    feat: create schema to validate the creation financial record data

---
+++ src/schema/CreateFinanciaSchema.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const CreateFinancialSchema = z.object({
+      body: z.object({
+            type: z.enum(
+                  ["INCOME", "EXPENSE"],
+                  "Tipo de registro é obrigatório",
+            ),
+            category: z
+                  .string("Categoria é obrigatória")
+                  .min(2, "Categoria deve ter mais de 2 caracteres"),
+            amount: z.number("Valor do registro é obrigatório"),
+            date: z.string("Data do registro é obrigatorio"),
+      }),
+});
