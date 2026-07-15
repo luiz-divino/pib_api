c59f4c4ef1759a6beb8ae704e521f5a8b3c6dbb5
luiz-divino <nandodivinodev@gmail.com>
2026-07-14 22:53:52 -0300

    feat: create the routes aplication
    - create the route to create a financial record
    - create the roue to do login

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,34 @@ import { makeCreateUserController } from "./factories/user/makeCreateUserControl
 import { validateSchema } from "./middlewares/validate";
 import { CreateUserSchema } from "./schema/CreateUserSchema";
 import { IMemoryUserRepository } from "./repositories/user/CreateUserRepository";
+import { makeAuthUserController } from "./factories/user/makeAuthUserController";
+import { AuthUserSchema } from "./schema/AuthUserSchema";
+import { ensuredAuthenticated } from "./middlewares/ensureAuthenticated";
+import { makeCreateFinancialRecord } from "./factories/financial/makeCreateFinancialRecordController";
+import { CreateFinancialSchema } from "./schema/CreateFinanciaSchema";
 const router = Router();
 const Imemory = new IMemoryUserRepository();
 const createUserController = makeCreateUserController();
+const authUserController = makeAuthUserController();
+const createFinancialController = makeCreateFinancialRecord();
 
 router.post(
       "/users",
       validateSchema(CreateUserSchema),
       createUserController.handle.bind(createUserController),
+);
+
+router.post(
+      "/session",
+      validateSchema(AuthUserSchema),
+      authUserController.handle.bind(authUserController),
+);
+
+router.post(
+      "/financial/records",
+      ensuredAuthenticated,
+      validateSchema(CreateFinancialSchema),
+      createFinancialController.handle.bind(createFinancialController),
 );
 
 export { router };
